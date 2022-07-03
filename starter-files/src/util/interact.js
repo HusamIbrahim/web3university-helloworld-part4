2bf518604b5d87268d69ee81fb857be12fdbcf66
Husam Ibrahim <huielbashir@gmail.com>
2022-07-03 22:49:43 +0200

    feat: setup connection through alchemy

--- starter-files/src/util/interact.js
+++ starter-files/src/util/interact.js
@@ -1,4 +1,14 @@
-//export const helloWorldContract;
+require('dotenv').config();
+
+const alchemyUrl = process.env.REACT_APP_ALCHEMY_URL;
+const { createAlchemyWeb3 } = require('@alch/alchemy-web3');
+const web3 = createAlchemyWeb3(alchemyUrl);
+
+const contractABI = require('../contract-abi.json');
+const contractAddress = process.env.REACT_APP_CONTRACT_ADDRESS;
+
+export const helloWorldContract = new web3.eth.Contract(contractABI, contractAddress);
+
 
 export const loadCurrentMessage = async () => { 
   
